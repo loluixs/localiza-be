b7884c8eda00820f9bc30950d754847e494c2c33
loluixs <cleanmec@gmail.com>
2023-02-23 00:11:33 -0300

    SPRINT-001-init

---
+++ src/main/java/SistemaLocalizaBEcom/example/Localiza/LocalizaApplication.java
@@ -0,0 +1,13 @@
+package SistemaLocalizaBEcom.example.Localiza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalizaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalizaApplication.class, args);
+	}
+
+}
